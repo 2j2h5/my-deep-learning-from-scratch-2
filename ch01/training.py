9382f5e7463c7a3cf321c8e6bb9e13704ae708e4
2j2h5 <chawun36@gmail.com>
2024-08-19 23:06:11 +0900

    Add TwoLayerNet but have bug

---
+++ ch01/training.py
@@ -0,0 +1,77 @@
+import sys
+sys.path.append("..")
+import numpy as np
+from common import optimizer as opt
+from dataset import spiral
+import matplotlib.pyplot as plt
+from ch01.two_layer_net import TwoLayerNet
+
+max_epoch = 300
+batch_size = 30
+hidden_size = 10
+learning_rate = 1.0
+
+x, t = spiral.load_data()
+model = TwoLayerNet(input_size=2, hidden_size=hidden_size, output_size=3)
+optimizer = opt.SGD(lr=learning_rate)
+
+data_size = len(x)
+max_iters = data_size // batch_size
+total_loss = 0
+loss_count = 0
+loss_list = []
+
+for epoch in range(max_epoch):
+    idx = np.random.permutation(data_size)
+    x = x[idx]
+    t = t[idx]
+
+    for iters in range(max_iters):
+        batch_x = x[iters*batch_size:(iters+1)*batch_size]
+        batch_t = t[iters*batch_size:(iters+1)*batch_size]
+
+        loss = model.forward(batch_x, batch_t)
+        model.backward()
+        optimizer.update(model.params, model.grads)
+
+        total_loss += loss
+        loss_count += 1
+
+        if (iters+1) % 10 == 0:
+            avg_loss = total_loss / loss_count
+            print(f"| Epoch {epoch+1} | Iterataion {iters+1} | Loss {avg_loss}")
+            loss_list.append(avg_loss)
+            total_loss, loss_count = 0, 0
+        
+""" x = np.arange(max_epoch)
+plt.plot(x, loss_list, label='loss')
+plt.xlabel("epochs")
+plt.ylabel("loss")
+plt.show() """
+
+# 학습 결과 플롯
+plt.plot(np.arange(len(loss_list)), loss_list, label='train')
+plt.xlabel('반복 (x10)')
+plt.ylabel('손실')
+plt.show()
+
+# 경계 영역 플롯
+h = 0.001
+x_min, x_max = x[:, 0].min() - .1, x[:, 0].max() + .1
+y_min, y_max = x[:, 1].min() - .1, x[:, 1].max() + .1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+X = np.c_[xx.ravel(), yy.ravel()]
+score = model.predict(X)
+predict_cls = np.argmax(score, axis=1)
+Z = predict_cls.reshape(xx.shape)
+plt.contourf(xx, yy, Z)
+plt.axis('off')
+
+# 데이터점 플롯
+x, t = spiral.load_data()
+N = 100
+CLS_NUM = 3
+markers = ['o', 'x', '^']
+for i in range(CLS_NUM):
+    plt.scatter(x[i*N:(i+1)*N, 0], x[i*N:(i+1)*N, 1], s=40, marker=markers[i])
+plt.show()
