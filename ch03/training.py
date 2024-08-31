24cb06dba6beb1e4933b2ed56fdeb6b44de4dfff
2j2h5 <chawun36@gmail.com>
2024-08-31 14:05:16 +0900

    fix typo

--- ch03/training.py
+++ ch03/training.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("..")
 from common.trainer import Trainer
 from common.optimizer import Adam
-from simpole_cbow import SimpleCBOW
+from simple_cbow import SimpleCBOW
 from common.util import preprocess, create_contexts_target, convert_one_hot
 
 window_size = 1
---
+++ common/sampler.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append('..')
+import numpy as np
+import collections
+
+class UnigramSampler:
+    def __init__(self, corpus, power, sample_size):
+        self.sample_size = sample_size
+        self.vocab_size = None
+        self.word_p = None
+
+        counts = collections.Counter()
+        for word_id in corpus:
+            counts[word_id] += 1
+
+        vocab_size = len(counts)
+        self.vocab_size = vocab_size
+
+        self.word_p = np.zeros(vocab_size)
+        for i in range(vocab_size):
+            self.word_p[i] = counts[i]
+
+        self.word_p = np.power(self.word_p, power)
+        self.word_p /= np.sum(self.word_p)
+
+    def get_negative_sample(self, target):
+        batch_size = target.shape[0]
+        negative_sample = np.random.choice(self.vocab_size, size=(batch_size, self.sample_size), replace=True, p=self.word_p)
+
+        return negative_sample
